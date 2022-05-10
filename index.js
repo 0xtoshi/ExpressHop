9c204a126378c901ba2fd304f42d3cae56786738
Gilang Dandung <42947375+0xtoshi@users.noreply.github.com>
2022-05-10 17:42:31 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : '',
+      database : 'hop'
+    }
+  });
+
+app.get('/', (req, res) => {
+  res.send(Airdrop.FindAddress())
+})
+
+app.get('/airdrop/address/:address', (req, res) => {
+    const address = req.params.address; 
+    knex.select()
+    .from('addressesMetadataAllUsers')
+    .join('finalDistribution', 'addressesMetadataAllUsers.address','finalDistribution.address')
+    .where('addressesMetadataAllUsers.address', address)
+    .then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        res.send('Error');
+    });
+    //res.send(sql);
+
+})
+
+app.get('/airdrop/totalTxs/:count', (req,res) => {
+    const count = req.params.count; 
+    knex.select()
+    .from('addressesMetadataAllUsers')
+    .where('totalTxs', count)
+    .then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        res.send('Error');
+        console.log(err);
+    });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

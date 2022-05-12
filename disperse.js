638b0259fa92a48ac95d08901a7ca21b2a7bc815
Gilang Dandung <42947375+0xtoshi@users.noreply.github.com>
2022-05-12 07:18:42 +0700

    Add Disperse

---
+++ disperse.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const fs = require('fs');
+const Web3 = require('web3');
+const web3 = new Web3('https://rpc-mainnet.matic.quiknode.pro/');
+var abiDecoder = require('abi-decoder');
+const ABI = [{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"recipients","type":"address[]"},{"name":"values","type":"uint256[]"}],"name":"disperseTokenSimple","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"recipients","type":"address[]"},{"name":"values","type":"uint256[]"}],"name":"disperseToken","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recipients","type":"address[]"},{"name":"values","type":"uint256[]"}],"name":"disperseEther","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}]
+abiDecoder.addABI(ABI);
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : '',
+      database : 'hop'
+    }
+  });
+
+const TxJson = fs.readFileSync('./TxDisperse1.json', 'utf8');
+const txData = JSON.parse(TxJson);
+const result = txData.result;
+
+console.log(result.length)
+/*
+result.forEach(async data => {
+    const decode_data = abiDecoder.decodeMethod(data.input);
+
+
+        if(decode_data !== undefined && decode_data.params[0].value[0] !== 0){
+
+          console.log(decode_data.params[0].value[0])
+     }else{
+
+     }
+
+     
+     
+});
+*/

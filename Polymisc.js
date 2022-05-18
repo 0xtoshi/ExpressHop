562632bb2c912a80c6a1522757150f74462cf9a7
Gilang Dandung <42947375+0xtoshi@users.noreply.github.com>
2022-05-19 02:02:36 +0700

    UPDATE CSV

--- Polymisc.js
+++ Polymisc.js
@@ -3,7 +3,8 @@ const ABI = [{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name"
 const abiDecoder = require('abi-decoder');
 const fs = require('fs');
 abiDecoder.addABI(ABI);
-const root_address = "0xa14ef0e1c1b3fe72706dc5f69ce0595e9dba0646";
+const endBlock = 26615721; //SNAPSHOT BLOCK
+const root_address = "0xec561dd73346f761d1c09f46999bc8ea1b1e95a2";
 const ContractAddress = {
     Dai : "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
     Weth : "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
@@ -23,7 +24,7 @@ var AddressList = [];
 
 (async() =>{
 
-    const PolygonScanAPI = await axios(`https://api.polygonscan.com/api?module=account&action=txlist&address=${root_address}&startblock=0&endblock=99999999&page=1&offset=1000&sort=DESC&apikey=F3TTPZBVHEWA7ZS1QWG2RP8YWGHK1ABCWT`);
+    const PolygonScanAPI = await axios(`https://api.polygonscan.com/api?module=account&action=txlist&address=${root_address}&startblock=0&endblock=${endBlock}&page=1&offset=1000&sort=DESC&apikey=F3TTPZBVHEWA7ZS1QWG2RP8YWGHK1ABCWT`);
    //console.log(PolygonScanAPI.data.result);
     for(let tx of PolygonScanAPI.data.result){
         if (tx.input == '0x') {
@@ -102,7 +103,8 @@ var AddressList = [];
         const Hop = await axios(`http://localhost:3000/airdrop/address/${finaladdress.address}`);
         if(Hop.data.data.hopUserTokens > 0)
         {
-            console.log(`[${root_address}](https://polygonscan.com/tx/${root_address})|[${Hop.data.data.address}](https://polygonscan.com/address/${Hop.data.data.address})|${finaladdress.amount}|[${finaladdress.hash}](https://polygonscan.com/tx/${finaladdress.hash})`);
+            //console.log(`[${root_address}](https://polygonscan.com/tx/${root_address})|[${Hop.data.data.address}](https://polygonscan.com/address/${Hop.data.data.address})|${finaladdress.amount}|[${finaladdress.hash}](https://polygonscan.com/tx/${finaladdress.hash})`);
+            console.log(finaladdress.address);
         }
     }
 })()

4cc6eaa4f9c7390162bd80d5cdae4006e8007135
Gilang Dandung <42947375+0xtoshi@users.noreply.github.com>
2022-05-23 01:58:23 +0000

    Update getTokenReward.js

--- getTokenReward.js
+++ getTokenReward.js
@@ -9,19 +9,19 @@ const axios = require('axios');
     var totalTx = 0;
     var totalVol = 0;
     for(let i=0; i<eligibleAddresses.length; i++){
-        const HopAPI = await axios(`https://airdrop-api.hop.exchange/v1/airdrop/${eligibleAddresses[i]}`);
+        const HopAPI = await axios(`http://localhost:3000/airdrop/address/${eligibleAddresses[i]}`);
         //console.log(HopAPI.data.data)
         elig.push(HopAPI.data.data);
-        total_reward += Number(HopAPI.data.data.totalTokens);
-        totalTx += Number(HopAPI.data.data.totalTxs); 
-        totalVol += Number(HopAPI.data.data.totalVolume);
+        total_reward += HopAPI.data.data.totalTokens;
+        totalTx += HopAPI.data.data.totalTxs; 
+        totalVol += HopAPI.data.data.totalVolume;
         //console.log(total_reward);
     }
     //console.log(total_token);
     console.log('Total Address in Eligible : '+ elig.length);
-    console.log('Total Rewarded : '+ Number(total_reward)/10**18);
-    console.log('Total AVG Txs : '+ Number(totalTx)/elig.length);
-    console.log('Total AVG Volume: '+ Number(totalVol/elig.length));
-    console.log('Your Reward : '+ Number(total_reward)/10**18 * 0.25);
+    console.log('Total Rewarded : '+ total_reward);
+    console.log('Total AVG Txs : '+ totalTx/elig.length);
+    console.log('Total AVG Volume: '+ totalVol/elig.length);
+    console.log('Your Reward : '+ total_reward);
 
 })();
